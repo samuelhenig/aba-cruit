2778f86c13d6cd30914e63ac594e44ac91f40550
samuelhenig <samuelhenig@gmail.com>
2026-07-29 17:09:26 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
       <Header />
       <Hero />
       <TrustBar />
-      <FeaturedOpportunity />
       <ReferralCTA />
       <TalentNetwork />
       <TwoPaths />
